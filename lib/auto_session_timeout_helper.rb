db9a964aa80e9043dc2d45ad4a109af4eb201974
Victor Kalutskiy <v1ktor_k@bk.ru>
2014-03-14 13:58:45 +1100

    Replace jQuery instead Ajax, jQuery always loaded.

--- lib/auto_session_timeout_helper.rb
+++ lib/auto_session_timeout_helper.rb
@@ -2,16 +2,18 @@ module AutoSessionTimeoutHelper
   def auto_session_timeout_js(options={})
     frequency = options[:frequency] || 60
     code = <<JS
-if (typeof(Ajax) != 'undefined') {
-  new Ajax.PeriodicalUpdater('', '/active', {frequency:#{frequency}, method:'get', onSuccess: function(e) {
-    if (e.responseText == 'false') window.location.href = '/timeout';
-  }});
-} else {
-  $.PeriodicalUpdater('/active', {minTimeout:#{frequency * 1000}, multiplier:0, method:'get', verbose:2}, function(remoteData, success) {
-    if (success == 'success' && remoteData == 'false')
-      window.location.href = '/timeout';
+function PeriodicalQuery() {
+  $.ajax({
+    url: '/active',
+    success: function(data) {
+      if(data == 'false'){
+        window.location.href = '/timeout';
+      }
+    }
   });
+  setTimeout(PeriodicalQuery, (#{frequency} * 1000));
 }
+setTimeout(PeriodicalQuery, (#{frequency} * 1000));
 JS
     javascript_tag(code)
   end
